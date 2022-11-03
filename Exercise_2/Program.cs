dd3f72d77837d666b5978257b8f2534cf6c58377
Furji UMY <furji.ft21@mail.umy.ac.id>
2022-11-03 16:40:16 +0700

    memperbaiki code dan menambahkan display

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -4,35 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise_2
+namespace contohaja
 {
     internal class Program
     {
         int[] furji = new int[50];
 
-        int n, i, j, val, flag, middle;
-
+        //Number of elements in array
+        int n, i, fi, val, flag;
         void input()
         {
             while (true)
             {
-                Console.WriteLine("Masukkan jumlah element pada Array :");
+                Console.Write("Masukkan Jumlah Elemen Array : ");
                 string s = Console.ReadLine();
                 n = Int32.Parse(s);
                 if (n <= 50)
                     break;
                 else
-                    Console.WriteLine("\nArray can have maximum 50 elemnts \n");
+                    Console.WriteLine("\nArray can have maximum 50 elements \n");
             }
             Console.WriteLine("\n********************");
             Console.WriteLine("Masukkan Element Array");
             Console.WriteLine("\n********************");
 
-            for (int i = 0; i < n; i++) ;
+            //get array elements
+            for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("<" + (i + 1) + ">");
-                string s = Console.ReadLine();
-                furji[i] = Int32.Parse(s);
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                furji[i] = Int32.Parse(s1);
             }
         }
         public void InsertionSort()
@@ -41,116 +42,65 @@ namespace Exercise_2
             {
                 val = furji[i];
                 flag = 0;
-                for (j = i - 1; j >= 0 && flag != 1;)
+                for (fi = i - 1; fi >= 0 && flag != 1;)
                 {
-                    if (val < furji[j])
+                    if (val < furji[fi])
                     {
-                        furji[j + 1] = furji[j];
-                        j--;
-                        furji[j + 1] = val;
+                        furji[fi + 1] = furji[fi];
+                        fi--;
+                        furji[fi + 1] = val;
                     }
                     else flag = 1;
                 }
             }
         }
-
-        public void MainMerge(int[] numbers, int left, int mid, int right)
+        public void Merge(int[] input, int left, int middle, int right)
         {
-            if (left < right)
-            {
-                int fi = left + (right - left) / 2;
-                mergesort(furji, left, mid);
-                mergesort(furji, mid + 1, right);
-                merge(furji, left, mid, right);
-            }
-        }
+            int[] leftArray = new int[middle - left + 1];
+            int[] rightArray = new int[right - middle];
 
-        // merge function performs sort and merge operations
-        // for mergesort function
-        static void merge(int[] Array, int left, int mid, int right)
-        {
-            // Create two temporary array to hold split 
-            // elements of main array 
-            int n1 = mid - left + 1; //no of elements in LeftArray
-            int n2 = right - mid;     //no of elements in RightArray    
-            int[] LeftArray = new int[n1];
-            int[] RightArray = new int[n2];
+            Array.Copy(input, left, leftArray, 0, middle - left + 1);
+            Array.Copy(input, middle + 1, rightArray, 0, right - middle);
 
-            for (int i = 0; i < n1; i++)
+            int i = 0;
+            int j = 0;
+            for (int k = left; k < right + 1; k++)
             {
-                LeftArray[i] = Array[left + i];
-            }
-
-            for (int i = 0; i < n2; i++)
-            {
-                RightArray[i] = Array[mid + i + 1];
-            }
-
-            // In below section x, y and z represents index number
-            // of LeftArray, RightArray and Array respectively
-            int x = 0, y = 0, z = left;
-            while (x < n1 && y < n2)
-            {
-                if (LeftArray[x] < RightArray[y])
+                if (i == leftArray.Length)
                 {
-                    Array[z] = LeftArray[x];
-                    x++;
+                    input[k] = rightArray[j];
+                    j++;
+                }
+                else if (j == rightArray.Length)
+                {
+                    input[k] = leftArray[i];
+                    i++;
+                }
+                else if (leftArray[i] <= rightArray[j])
+                {
+                    input[k] = leftArray[i];
+                    i++;
                 }
                 else
                 {
-                    Array[z] = RightArray[y];
-                    y++;
+                    input[k] = rightArray[j];
+                    j++;
                 }
-                z++;
             }
+        }
 
-            // Copying the remaining elements of LeftArray
-            while (x < n1)
+        void display()
+        {
+            Console.WriteLine("\n-----------------------");
+            Console.WriteLine(" Sorted array elements ");
+            Console.WriteLine("-------------------------");
+
+            for (int j = 0; j < n; j++)
             {
-                Array[z] = LeftArray[x];
-                x++;
-                z++;
+                Console.WriteLine(furji[fi]);
             }
 
-            // Copying the remaining elements of RightArray
-            while (y < n2)
-            {
-                Array[z] = RightArray[y];
-                y++;
-                z++;
-            }
         }
 
-        // function to print array
-        static void PrintArray(int[] Array)
-        {
-            int n = Array.Length;
-            for (int i = 0; i < n; i++)
-                Console.Write(Array[i] + " ");
-            Console.Write("\n");
-        }
-
-        // test the code
-        static void Main(string[] args)
-        {
-            int[] MyArray = { 10, 1, 23, 50, 4, 9, -4 };
-            int n = MyArray.Length;
-            Console.Write("Original Array\n");
-            PrintArray(MyArray);
-
-            mergesort(MyArray, 0, n - 1);
-            Console.Write("\nSorted Array\n");
-            PrintArray(MyArray);
-        }
-
-        private static void mergesort(int[] myArray, int v1, int v2)
-        {
-            throw new NotImplementedException();
-        }
-
-        static void fi(string[] args)
-        {
-         
-        }
     }
 }

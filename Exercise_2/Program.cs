0f933bd95714e0ba86dea14a2f393d7a7a99b5cf
Furji UMY <furji.ft21@mail.umy.ac.id>
2022-11-03 15:52:04 +0700

    add console type string in method

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -8,8 +8,24 @@ namespace Exercise_2
 {
     internal class Program
     {
+        int[] furji = new int[50];
+
+        int n, i, j, val, flag;
+
+        void input()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the number of element in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 50)
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
+         
         }
     }
 }
